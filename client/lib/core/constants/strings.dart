a8c0d9955a75bd02e7b5805cd1b438b77429e322
Jagmeet236 <jagmeetsingh236@gmail.com>
2025-05-28 23:55:51 +0530

    added the server strings for ios and android

--- client/lib/core/constants/strings.dart
+++ client/lib/core/constants/strings.dart
@@ -6,4 +6,5 @@ const textSignIn = 'Sign In';
 const alreadyHaveAnAccount = 'Already have an account?';
 const dontHaveAnAccount = "Don't have an account?";
 const textForgotPassword = 'Forgot Password?';
-const kBaseURL = 'http://10.0.2.2:8000';
+const kAndroid = 'http://10.0.2.2:8000';
+const kIOS = 'http://127.0.0.1:8000';
