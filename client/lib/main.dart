54b9b660ae21ac3a487fd397b61d9bfeca9687e0
Jagmeet236 <jagmeetsing236@gmail.com>
2026-03-02 19:49:10 +0530

    trigger flutter ci

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -29,6 +29,7 @@ class MusicApp extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final currentUser = ref.watch(currentUserNotifierProvider);
     return MaterialApp(
+      /// title of the app
       title: 'Music App',
       theme: AppTheme.darkThemeMode,
 
